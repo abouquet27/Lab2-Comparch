8b5c2a21149fe7fa30ffafd8e88650d0a0fc9cf1
abouquet27 <adrien.bouquet@epfl.ch>
2022-05-20 12:06:56 +0200

    double pipelined nealry done

--- Lab 5 Pipelining of an Arithmetic/project_template/vhdl/arith_unit.vhd
+++ Lab 5 Pipelining of an Arithmetic/project_template/vhdl/arith_unit.vhd
@@ -88,11 +88,12 @@ end combinatorial;
 -- =============================================================================
 
 architecture one_stage_pipeline of arith_unit is
-signal s_bc, s_a_square, s_to_square : unsigned(15 downto 0);
+	signal s_bc, s_a_square, s_to_square : unsigned(15 downto 0);
 	signal s_square : unsigned(31 downto 0);
 	signal s_add1, s_add2, s_mult : unsigned(15 downto 0);
-	signal s_to_square_bis, s_mult_bis : unsigned(15 downto 0);
+	signal s_mult2, s_mult_bis, s_mult_bis2, s_add2_bis : unsigned(15 downto 0);
 	signal s_mult_result: unsigned(31 downto 0);
+	signal A_prime, B_prime: unsigned(7 downto 0);
 	
     component multiplier
         port(
@@ -109,33 +110,33 @@ signal s_bc, s_a_square, s_to_square : unsigned(15 downto 0);
     end component;
 
 begin
-	mult1: multiplier
-	PORT MAP(A => B,
-	B => C,
-	P => s_bc);
-
+	
+	A_prime <= A when sel = '1' else B;
+	B_prime <= A when sel = '1' else C;
 
 	mult2: multiplier
-	PORT MAP(A => A,
-	B => A,
-	P => s_a_square);
+	PORT MAP(A => A_prime,
+	B => B_prime,
+	P => s_mult);
 	
-	s_to_square <= s_a_square when sel = '1' else s_bc;
-
 
 	s_add1 <= x"00" & A when sel = '0' else "0000000" & A & "0";
 	s_add2 <= s_add1 + B;
 
-	s_mult <= s_bc when sel = '0' else s_add2;
-
+	s_mult2 <= s_mult when sel = '0' else s_add2;
+	
+		
 	dff: process(clk, reset_n)is 
 	begin
-		if (rising_edge(clk)) then 
-			s_mult_bis <= s_mult;
-			s_to_square_bis <= s_to_square;
-		elsif(reset_n = '0') then
+		if(reset_n = '0') then
+			s_mult_bis2 <= x"0000";
 			s_mult_bis <= x"0000";
-			s_to_square_bis <= x"0000";
+			s_add2_bis <= x"0000";
+		elsif (rising_edge(clk)) then 
+			s_mult_bis2 <= s_mult2;
+			s_mult_bis <= s_mult;
+			s_add2_bis <= s_add2;
+		
 		end if;
 			
 	end process;
@@ -144,14 +145,14 @@ begin
 	
 
 	mult3: multiplier16
-	PORT MAP(A => s_to_square_bis ,
-	B => s_to_square_bis,
+	PORT MAP(A => s_mult_bis ,
+	B => s_mult_bis,
 	P => s_square);
 	
 	
 	mult4: multiplier16
-	PORT MAP(A => s_add2,
-	B => s_mult,
+	PORT MAP(A => s_add2_bis,
+	B => s_mult_bis2,
 	P => s_mult_result);
 
 	D <= s_mult_result + s_square;
@@ -164,7 +165,13 @@ end one_stage_pipeline;
 -- ============================ 2 STAGE PIPELINE I =============================
 -- =============================================================================
 
-architecture two_stage_pipeline_1 of arith_unit is
+architecture two_stage_pipeline_1 of arith_unit is	
+	signal s_square, s_square_bis : unsigned(31 downto 0);
+	signal s_add1, s_add2, s_mult : unsigned(15 downto 0);
+	signal s_mult2, s_mult_bis, s_mult_bis2, s_add2_bis : unsigned(15 downto 0);
+	signal s_mult_result, s_mult_result_bis: unsigned(31 downto 0);
+	signal A_prime, B_prime: unsigned(7 downto 0);
+	
     component multiplier
         port(
             A, B : in  unsigned(7 downto 0);
@@ -180,6 +187,66 @@ architecture two_stage_pipeline_1 of arith_unit is
     end component;
 
 begin
+	
+	A_prime <= A when sel = '1' else B;
+	B_prime <= A when sel = '1' else C;
+
+	mult2: multiplier
+	PORT MAP(A => A_prime,
+	B => B_prime,
+	P => s_mult);
+	
+
+	s_add1 <= x"00" & A when sel = '0' else "0000000" & A & "0";
+	s_add2 <= s_add1 + B;
+
+	s_mult2 <= s_mult when sel = '0' else s_add2;
+	
+		
+	dff: process(clk, reset_n)is 
+	begin
+		if(reset_n = '0') then
+			s_mult_bis2 <= x"0000";
+			s_mult_bis <= x"0000";
+			s_add2_bis <= x"0000";
+		elsif (rising_edge(clk)) then 
+			s_mult_bis2 <= s_mult2;
+			s_mult_bis <= s_mult;
+			s_add2_bis <= s_add2;
+		
+		end if;
+			
+	end process;
+	
+
+	mult3: multiplier16
+	PORT MAP(A => s_mult_bis ,
+	B => s_mult_bis,
+	P => s_square);
+	
+	
+	mult4: multiplier16
+	PORT MAP(A => s_add2_bis,
+	B => s_mult_bis2,
+	P => s_mult_result);
+		
+	dff1: process(clk, reset_n)is 
+	begin
+		if(reset_n = '0') then
+			s_mult_result_bis <= x"00000000";
+			s_square_bis <= x"00000000";
+		elsif (rising_edge(clk)) then 
+			s_mult_result_bis <= s_mult_result;
+			s_square_bis <= s_square;
+		
+		end if;
+			
+	end process;
+
+	D <= s_mult_result_bis + s_square_bis;
+	done <= start;
+	
+	
 end two_stage_pipeline_1;
 
 -- =============================================================================
